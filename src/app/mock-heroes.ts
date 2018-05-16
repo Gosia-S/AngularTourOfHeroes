1b7def9c858d69e548f3180a7088e918d26004f3
Gosia <malgorzata.d.sikora@gmail.com>
2018-05-16 14:40:36 -0700

    angular tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', img:'mrnice.jpg'},
+  { id: 12, name: 'Narco', img:''},
+  { id: 13, name: 'Bombasto', img:'' },
+  { id: 14, name: 'Celeritas', img:'' },
+  { id: 15, name: 'Magneta', img:'' },
+  { id: 16, name: 'RubberMan', img:'' },
+  { id: 17, name: 'Dynama', img:'' },
+  { id: 18, name: 'Dr IQ', img:'' },
+  { id: 19, name: 'Magma', img:'' },
+  { id: 20, name: 'Tornado', img:'' }
+];
